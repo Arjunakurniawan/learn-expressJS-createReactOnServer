a04aab5ff5a9e9d7871f11ec3f027090704a6081
ArjunaKurniawan <arjunakurniawan686@gmail.com>
2024-11-12 20:30:18 +0700

    task: next create react on server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = "3002";
+
+// menggunakan middleware
+app.use("/", express.static(path.join(__dirname)));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// app.use("/image", function (req, res) {
+//   res.sendFile(express.static(path.join(__dirname, "/img")));
+// });
+
+//study kasus tanpa middleware
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.get("/image", (req, res) => {
+//   res.sendFile(
+//     __dirname + "/img/mihawk-sword-one-piece-uhdpaper.com-4K-6.795.jpg"
+//   );
+// });
+
+app.listen(port, function () {
+  console.log(`server running in http://localhost:${port}`);
+});
